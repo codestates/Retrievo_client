f260f40fbc531f972a3e7a30f857449d5283ba99
dkje <developer.kje@gmail.com>
2020-12-09 11:26:24 +0900

    [RE-173] | adding react-router-dom & lodash & charJs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
 import { ChakraProvider, Button } from "@chakra-ui/react";
+// import { DragDropContext, Draggable } from "react-beautiful-dnd";
 import theme from "./styles/theme";
 
 const App: React.FC<Record<string, never>> = () => (
   <ChakraProvider theme={theme}>
-    <Button>테스트 버튼</Button>
+    <Button>ㅠ.ㅠ</Button>
   </ChakraProvider>
 );
 
 export default App;
+
+// Type 'Element' is not assignable to type 'DraggableChildrenFn & ReactPortal'.
