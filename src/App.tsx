29433e5c375c912adfb8be302cc27a08f789bd46
dkje <developer.kje@gmail.com>
2020-12-09 23:19:18 +0900

    [RE-175] | storybook init

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Button } from "@chakra-ui/react";
 import theme from "./styles/theme";
 
 const App: React.FC<Record<string, never>> = () => {
   return (
     <>
-      <ChakraProvider theme={theme}>앱</ChakraProvider>
+      <ChakraProvider theme={theme}>
+        <Button>다른가?</Button>
+      </ChakraProvider>
     </>
   );
 };
