9c93d60d886488e29df1a0cb1a070ecdd0ab1d2a
dongoc <dongoc21hj@gmail.com>
2020-12-10 21:46:56 +0900

    [RE-177] | before pull upstream

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import React from "react";
 import { ChakraProvider, Text } from "@chakra-ui/react";
 import theme from "./styles/theme";
 // import Button from "./components/Button";
-import Heading, { headingStyle } from "./components/Heading";
+// import Heading from "./components/Heading";
 
 const App: React.FC<Record<string, never>> = () => {
   return (
     <>
       <ChakraProvider theme={theme}>
-        <Heading headingType={headingStyle.board}>Retrievo</Heading>
         <Text>hello</Text>
         <p>World</p>
       </ChakraProvider>
