b1380f8811a8ee2b0f495fea7ce247e7814d3e09
Si Choi <0334pc@gmail.com>
2020-12-11 01:29:00 +0900

    [RE-180] | Fixed RoundButton Errors

--- src/components/RoundButton/RoundButton.stories.tsx
+++ src/components/RoundButton/RoundButton.stories.tsx
@@ -1,21 +1,103 @@
 import React from "react";
-import RoundButton, { roundButtonProps, roundBtnColors } from "./index";
+import RoundButton, {
+  roundButtonProps,
+  RoundButtonColor,
+  SizeType,
+  IconType,
+} from "./index";
 
 export const NormalButton = (args: roundButtonProps): React.ReactElement => (
   <RoundButton {...args} />
 );
 NormalButton.args = {
-  buttonType: "yellow",
+  buttonColor: "primary",
+};
+
+export const RoundIconButton = (args: roundButtonProps): React.ReactElement => (
+  <RoundButton {...args} />
+);
+RoundIconButton.args = {
+  buttonColor: "primary",
+};
+
+export const GoogleButton = (args: roundButtonProps): React.ReactElement => (
+  <RoundButton {...args} />
+);
+GoogleButton.args = {
+  buttonColor: "white",
+  bgColor: "white",
+  iconType: "google",
+  shadow: "xl",
+  fontSize: "lg",
+};
+
+export const GithubButton = (args: roundButtonProps): React.ReactElement => (
+  <RoundButton {...args} />
+);
+GithubButton.args = {
+  buttonColor: "black",
+  borderColor: "gray.200",
+  iconType: "github",
+  shadow: "xl",
+  fontSize: "xl",
+  color: "white",
+};
+
+export const PlusButton = (args: roundButtonProps): React.ReactElement => (
+  <RoundButton {...args} />
+);
+PlusButton.args = {
+  borderColor: "gray.200",
+  iconType: "plus",
+  shadow: "xl",
+  fontSize: "xl",
+  color: "white",
+  bgColor: "gray.500",
+};
+
+export const ProfileUploadButton = (
+  args: roundButtonProps
+): React.ReactElement => <RoundButton {...args} />;
+ProfileUploadButton.args = {
+  buttonColor: "primary",
+  iconType: "camera",
+  shadow: "xl",
+  fontSize: "xl",
+  color: "white",
 };
 
 const ButtonStories = {
   title: "components/RoundButton",
   component: RoundButton,
   argTypes: {
-    buttonType: {
+    buttonColor: {
       control: {
         type: "select",
-        options: Object.keys(roundBtnColors),
+        options: Object.keys(RoundButtonColor),
+      },
+    },
+    size: {
+      control: {
+        type: "select",
+        options: Object.keys(SizeType),
+      },
+    },
+    iconType: {
+      control: {
+        type: "select",
+        options: Object.keys(IconType),
+      },
+    },
+    fontSize: {
+      control: {
+        type: "select",
+        options: Object.keys(SizeType),
+      },
+    },
+    shadow: {
+      control: {
+        type: "select",
+        options: Object.keys(SizeType),
       },
     },
   },
