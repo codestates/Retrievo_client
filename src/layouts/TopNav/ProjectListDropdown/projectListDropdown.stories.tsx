650a508e3d0175eb6754b1e8f1497489e100b76e
dongoc <dongoc21hj@gmail.com>
2020-12-12 13:29:36 +0900

    [RE-193] | add topNav and projectListDropdown

---
+++ src/layouts/TopNav/ProjectListDropdown/projectListDropdown.stories.tsx
@@ -0,0 +1,40 @@
+export {};
+/* eslint-disable no-unused-vars */
+// import React from "react";
+// import ProjectListDropdown, { ProjectListDropdownPropsType } from ".";
+
+// export const projectListDropdown: React.FC<ProjectListDropdownPropsType> = ({
+//   ...args
+// }) => <ProjectListDropdown {...args} />;
+// projectListDropdown.args = {
+//   projects: [
+//     {
+//       id: "1",
+//       name: "Rock Paper Queens",
+//     },
+//     {
+//       id: "2",
+//       name: "My Blueberry",
+//     },
+//     {
+//       id: "3",
+//       name: "Current Project",
+//     },
+//     {
+//       id: "4",
+//       name: "Retrievo",
+//     },
+//   ],
+//   currentProject: {
+//     id: "4",
+//     name: "Retrievo",
+//   },
+//   onProjectSelect: (id: string) => console.log(id),
+// };
+
+// const ProjectListDropdownStories = {
+//   title: "layouts/TopNav/ProjectListDropdown",
+//   component: ProjectListDropdown,
+// };
+
+// export default ProjectListDropdownStories;
---
+++ src/layouts/TopNav/topNav.stories.tsx
@@ -0,0 +1,60 @@
+export {};
+/* eslint-disable no-unused-vars */
+// import React from "react";
+// import TopNav from "./index";
+
+// export const topNav = ({ ...args }): React.ReactElement => <TopNav {...args} />;
+// topNav.args = {
+//   projects: [
+//     {
+//       id: "1",
+//       name: "Rock Paper Queens",
+//     },
+//     {
+//       id: "2",
+//       name: "My Blueberry",
+//     },
+//     {
+//       id: "3",
+//       name: "Current Project",
+//     },
+//     {
+//       id: "4",
+//       name: "Retrievo",
+//     },
+//   ],
+//   currentProject: {
+//     id: "4",
+//     name: "Retrievo",
+//   },
+//   onProjectSelect: (id: string) => console.log(id),
+//   avatars: [
+//     {
+//       name: "stupy",
+//       src:
+//         "https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OHx8ZG9nfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60",
+//     },
+//     {
+//       name: "prettie",
+//       src:
+//         "https://images.unsplash.com/photo-1592159371936-61a70cbeb5f7?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTR8fGhhbXN0ZXJ8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60",
+//     },
+//     {
+//       name: "bunny",
+//       src:
+//         "https://images.unsplash.com/photo-1573316364756-33b34fad4fcb?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTJ8fHJhYmJpdHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60",
+//     },
+//     {
+//       name: "cuttie pie",
+//       src:
+//         "https://images.unsplash.com/photo-1561948955-570b270e7c36?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8Y2F0fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60",
+//     },
+//   ],
+// };
+
+// const TopNavStories = {
+//   title: "layouts/TopNav",
+//   component: TopNav,
+// };
+
+// export default TopNavStories;
