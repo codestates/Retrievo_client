533ac9a22c3a0e49f342ea465226871a6afa0bc6
pbkim0725 <pbkim0725@gmail.com>
2020-12-12 22:08:12 +0900

    [RE-204] | [Added] | mytasks & activityStream

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import React from "react";
-import { ChakraProvider, Text } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./styles/theme";
 // import Button from "./components/Button";
 // import Heading, { headingStyle } from "./components/Heading";
+import ActivityStream from "./pages/Dashboard/ActivityStream";
 
 const App: React.FC<Record<string, never>> = () => {
   return (
     <>
       <ChakraProvider theme={theme}>
-        {/* <Heading headingType={headingStyle.board}>Retrievo</Heading> */}
-        <Text>hello</Text>
-        <p>World</p>
+        <ActivityStream />
       </ChakraProvider>
     </>
   );
