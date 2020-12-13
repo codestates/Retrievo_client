a258059797c63e8d43d2b5befe7dc8ad2f6c60cf
pbkim0725 <pbkim0725@gmail.com>
2020-12-13 15:33:02 +0900

    [RE-215] | in progress, templates done

--- src/pages/ProjectSetting/BasicDetail/index.tsx
+++ src/pages/ProjectSetting/BasicDetail/index.tsx
@@ -1,18 +1,10 @@
 import React from "react";
 
-import * as Yup from "yup";
-import CustomForm from "../../../components/Form";
-import { ProjectNameInput } from "./ProjectNameInput";
-
-const initialValues = {};
-const validationSchema = {};
-const onSubmit = {};
-
 export const BasicDetail: React.FC = () => {
   return (
-    <CustomForm initialValues={} validationSchema={} onSubmit={}>
-      <ProjectNameInput />;
-    </CustomForm>
+    <>
+      <h1>hello</h1>
+    </>
   );
 };
 
