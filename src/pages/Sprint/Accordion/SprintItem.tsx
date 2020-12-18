4a375a522df8abf76df61a8ca82848c53299bd2f
pbkim0725 <pbkim0725@gmail.com>
2020-12-18 21:01:49 +0900

    [RE-266] | deciding on possible solution for dnd algo

--- src/pages/Sprint/Accordion/SprintItem.tsx
+++ src/pages/Sprint/Accordion/SprintItem.tsx
@@ -51,7 +51,7 @@ export const SprintItem: React.FC<Record<string, any>> = ({
   const location = useLocation();
   const toast = useToast();
   const projectId = location.pathname.split("/").pop() || "";
-  const [selected] = useState<boolean>(row === 0);
+  const [selected, setSelected] = useState<boolean>(row === 0);
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [
     updateSprintMutation,
@@ -138,147 +138,118 @@ export const SprintItem: React.FC<Record<string, any>> = ({
     });
   };
 
-  /*
- (cache, { data }) => {
-        console.log(data);
-        if (!data) return;
-        const cacheId = cache.identify(data);
-        console.log(cacheId);
-        cache.modify({
-          id: cache.identify(data.deleteSprint),
-          fields: {
-            getSprints: (existingSprintRefs, { readField }) => {
-              console.log(cache.identify(data.deleteSprint));
-              console.log(existingSprintRefs);
-              const hello = existingSprintRefs.sprints.filter(
-                (sprintRef: any) => {
-                  return sprintData.id !== readField("id", sprintRef);
-                }
-              );
-
-              return { ...existingSprintRefs, sprints: hello };
-            },
-          },
-        });
-  */
-
   return (
     <Draggable key={sprintData.id} draggableId={sprintData.id} index={row}>
       {(provided) => {
         return (
-          <Accordion
-            allowToggle
-            // onChange={() => setSelected(!selected)}
-            // defaultIndex={row}
+          <AccordionItem
+            ref={provided.innerRef}
+            {...provided.dragHandleProps}
+            {...provided.draggableProps}
           >
-            <AccordionItem>
-              <Flex
-                ref={provided.innerRef}
-                {...provided.dragHandleProps}
-                {...provided.draggableProps}
-                alignItems="center"
-                // bgColor={row === 0 ? "primary.400" : "achromatic.100"}
-                p={2}
-              >
-                <Center w="40px" h="40px" overflow="hidden">
-                  <AccordionButton
-                    p="8px"
-                    w="100%"
-                    h="100%"
-                    display="flex"
-                    justifyContent="center"
-                    _hover={{
-                      bg: "primary.400",
-                      borderRadius: "9999px",
-                      transition: "ease 0.3s",
-                    }}
-                    _focus={{ outline: "none" }}
-                  >
-                    <AccordionIcon fontSize="2rem" />
-                  </AccordionButton>
-                </Center>
-                <Box flex="1" ml={3} textAlign="left">
-                  {sprintData.title}
-                </Box>
-                <Flex
-                  justifyContent="flex-end"
-                  p={3}
-                  visibility={selected ? "visible" : "hidden"}
+            <Flex
+              alignItems="center"
+              bgColor={row === 0 ? "primary.400" : "achromatic.100"}
+              p={2}
+            >
+              <Center w="40px" h="40px" overflow="hidden">
+                <AccordionButton
+                  p="8px"
+                  w="100%"
+                  h="100%"
+                  display="flex"
+                  justifyContent="center"
+                  _hover={{
+                    bg: "primary.400",
+                    borderRadius: "9999px",
+                    transition: "ease 0.3s",
+                  }}
+                  _focus={{ outline: "none" }}
                 >
-                  <Menu>
-                    <MenuButton
-                      as={Button}
-                      p={3}
-                      border="none"
-                      backgroundColor="transparent"
-                      outline="none"
-                      _hover={{
-                        outline: "none",
-                        backgroundColor: "transparent",
-                      }}
-                      _focus={{
-                        outline: "none",
-                        backgroundColor: "transparent",
-                      }}
+                  <AccordionIcon fontSize="2rem" />
+                </AccordionButton>
+              </Center>
+              <Box flex="1" ml={3} textAlign="left">
+                {sprintData.title}
+              </Box>
+              <Flex
+                justifyContent="flex-end"
+                p={3}
+                visibility={selected ? "visible" : "hidden"}
+              >
+                <Menu>
+                  <MenuButton
+                    as={Button}
+                    p={3}
+                    border="none"
+                    backgroundColor="transparent"
+                    outline="none"
+                    _hover={{
+                      outline: "none",
+                      backgroundColor: "transparent",
+                    }}
+                    _focus={{
+                      outline: "none",
+                      backgroundColor: "transparent",
+                    }}
+                    color="achromatic.600"
+                    fontSize="xl"
+                  >
+                    <BsThreeDotsVertical />
+                  </MenuButton>
+                  <MenuList>
+                    <MenuItem onClick={onOpen}>Update Sprint</MenuItem>
+                    <ModalLayout
+                      isOpen={isOpen}
+                      onOpen={onOpen}
+                      onClose={onClose}
+                      footer={false}
+                      title="Update Sprint"
+                      buttonText="Update Sprint"
+                      bgColor="primary.400"
                       color="achromatic.600"
-                      fontSize="xl"
+                      borderRadius="9999px"
+                      display="none"
                     >
-                      <BsThreeDotsVertical />
-                    </MenuButton>
-                    <MenuList>
-                      <MenuItem onClick={onOpen}>Update Sprint</MenuItem>
-                      <ModalLayout
-                        isOpen={isOpen}
-                        onOpen={onOpen}
-                        onClose={onClose}
-                        footer={false}
-                        title="Update Sprint"
-                        buttonText="Update Sprint"
-                        bgColor="primary.400"
-                        color="achromatic.600"
-                        borderRadius="9999px"
-                        display="none"
-                      >
-                        <Box mb={3}>
-                          <CustomForm
-                            initialValues={{
-                              sprintName: "",
-                              description: "",
-                            }}
-                            buttonPosition="right"
-                            isSubmitButton
-                            submitBtnName="Update Sprint"
-                            onSubmit={handleUpdateSprint}
-                          >
-                            <Box lineHeight={8}>
-                              <Box p={2}>
-                                <InputField
-                                  label="Sprint Name"
-                                  name="sprintName"
-                                  placeholder={sprintData.title}
-                                />
-                              </Box>
-                              <Box p={2} mb={6}>
-                                <TextAreaField
-                                  label="Sprint Description"
-                                  name="description"
-                                  placeholder={sprintData.description}
-                                />
-                              </Box>
+                      <Box mb={3}>
+                        <CustomForm
+                          initialValues={{
+                            sprintName: "",
+                            description: "",
+                          }}
+                          buttonPosition="right"
+                          isSubmitButton
+                          submitBtnName="Update Sprint"
+                          onSubmit={handleUpdateSprint}
+                        >
+                          <Box lineHeight={8}>
+                            <Box p={2}>
+                              <InputField
+                                label="Sprint Name"
+                                name="sprintName"
+                                placeholder={sprintData.title}
+                              />
                             </Box>
-                          </CustomForm>
-                        </Box>
-                      </ModalLayout>
-                      <MenuItem onClick={handleDeleteSprint}>
-                        Delete Sprint
-                      </MenuItem>
-                    </MenuList>
-                  </Menu>
-                </Flex>
+                            <Box p={2} mb={6}>
+                              <TextAreaField
+                                label="Sprint Description"
+                                name="description"
+                                placeholder={sprintData.description}
+                              />
+                            </Box>
+                          </Box>
+                        </CustomForm>
+                      </Box>
+                    </ModalLayout>
+                    <MenuItem onClick={handleDeleteSprint}>
+                      Delete Sprint
+                    </MenuItem>
+                  </MenuList>
+                </Menu>
               </Flex>
-              <TaskList taskData={tasks} />
-            </AccordionItem>
-          </Accordion>
+            </Flex>
+            <TaskList taskData={tasks} />
+          </AccordionItem>
         );
       }}
     </Draggable>
--- src/pages/Sprint/Accordion/index.tsx
+++ src/pages/Sprint/Accordion/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Box } from "@chakra-ui/react";
+import { Accordion, Box } from "@chakra-ui/react";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 import { useLocation } from "react-router-dom";
 import SprintItem from "./SprintItem";
@@ -12,27 +12,47 @@ import Spinner from "../../../components/Spinner";
 export const Sprints: React.FC = () => {
   const location = useLocation();
   const projectId = location.pathname.split("/").pop() || "";
-  const [sprints, setSprints] = useState<Record<string, any>[]>([]);
+  // const [sprints, setSprints] = useState<Record<string, any>[]>([]);
   const { data, loading, error } = useGetSprintsQuery({
     variables: { projectId },
     fetchPolicy: "cache-and-network",
   });
+  const sprints = data?.getSprints.sprints;
 
   const [updateSprintMutation] = useUpdateSprintMutation();
 
-  useEffect(() => {
-    if (data?.getSprints.sprints) {
-      setSprints(data?.getSprints.sprints);
-    }
-    console.log("howmany times am i rendering?");
-  }, [data]);
+  // useEffect(() => {
+  //   if (data?.getSprints.sprints) {
+  //     setSprints(data?.getSprints.sprints);
+  //   }
+  //   console.log("howmany times am i rendering?");
+  // }, [data]);
 
   if (loading) return <Spinner />;
-  if (!data?.getSprints.sprints) return <Spinner />;
+  if (!sprints) return <Spinner />;
 
   const onDragEnd = (result: Record<string, any>) => {
     if (!result.destination) return;
     console.log("result", result);
+
+    /* fe dnd logic */
+    const items = Array.from(sprints);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    /*
+        모든 데이터에 대한 로우 변경은 서버에 들어가면 알아서 자동 처리 됨.
+        문제는 캐쉬임. 캐쉬도 결국에는  일괄 업데이트를 해주어야함
+        위의 fe dnd 로직이 array 자체는 바뀐 어레이를 되돌려주고 있다.
+        문제는 캐시의 데이터 형태랑 저 위에 애들이랑 안맞는다는 점.
+        (1)물론 캐쉬에도 id가 남아있기 때문에 스플릿 하고 맵을 하면 어찌저찌 가능하긴 함.
+        단 스프린트 자체는 무한대로 늘어날 수 있어서 loop 을 하면 성능에 영향이 올 수 있음.
+
+        (2) 업데이트가 끝나면 바로 refetching 을 해오는 방식도 존제. 문제는 db 에 대한 자료요청량이 늘어난다.
+
+        (3) dnd 결과에 따라 일괄적으로 처리할 수 있는 방식이 존재할 것인가?
+        아니면 변경된 캐쉬를 통해 빠르게 업데이트를 해야하는가.
+      */
 
     updateSprintMutation({
       variables: {
@@ -43,7 +63,6 @@ export const Sprints: React.FC = () => {
         },
       },
       update: (cache, { data }) => {
-        console.log(data);
         if (!data) return;
         if (!data.updateSprint.sprint) return;
         const cacheId = cache.identify(data.updateSprint.sprint);
@@ -57,31 +76,38 @@ export const Sprints: React.FC = () => {
         });
       },
     });
-    const items = Array.from(sprints); // items = sprints
-    // const [reorderedItem] = items.splice(result.source.index, 1);
-    // items.splice(result.destination.index, 0, reorderedItem);
-    setSprints(items);
+    // setSprints(items);
   };
 
   console.log(data?.getSprints.sprints);
   return (
     <DragDropContext onDragEnd={onDragEnd}>
-      {/* <pre>{JSON.stringify(sprints, null, 2)}</pre> */}
       <Box>
         <Droppable droppableId="droppableSprint">
           {(provided) => (
             <Box {...provided.droppableProps} ref={provided.innerRef}>
-              {sprints.map((sprint: any) => {
-                return (
-                  <SprintItem
-                    key={sprint.id}
-                    sprintData={sprint}
-                    row={sprint.row}
-                    tasks={sprint.task}
-                  />
-                );
-              })}
+              <Accordion
+                allowToggle
+                // onChange={() => setSelected(!selected)}
+                defaultIndex={0}
+              >
+                {sprints ? (
+                  sprints.map((sprint: any) => {
+                    return (
+                      <SprintItem
+                        key={sprint.id}
+                        sprintData={sprint}
+                        row={sprint.row}
+                        tasks={sprint.task}
+                      />
+                    );
+                  })
+                ) : (
+                  <Box>Error</Box>
+                )}
+              </Accordion>
               {provided.placeholder}
+              <pre>{JSON.stringify(sprints, null, 2)}</pre>
             </Box>
           )}
         </Droppable>
