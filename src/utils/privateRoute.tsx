dfc1535d6b57be7dc78945033339d7652baf7ea9
dongoc <dongoc21hj@gmail.com>
2020-12-19 15:55:57 +0900

    [RE-281] | before conflict

--- src/utils/privateRoute.tsx
+++ src/utils/privateRoute.tsx
@@ -1,8 +1,6 @@
 /* eslint-disable indent */
 import React from "react";
-import useCookies from "react-cookie/es6/useCookies";
 import { Route, Redirect, useParams, useLocation } from "react-router-dom";
-import { useGetProjectLazyQuery } from "../generated/graphql";
 import ROUTES from "./RoutePath";
 import useQuery from "../hooks/useQuery";
 
