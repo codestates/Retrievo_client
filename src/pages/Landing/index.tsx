48e9dabdeb2784b9cf851c975346655cd65f47b5
dkje <developer.kje@gmail.com>
2020-12-21 18:31:10 +0900

    [routefix] | fix board layout

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -530,7 +530,6 @@ const Landing: React.FC<Record<string, never>> = () => {
         />
         <Flex flexDir="column" mt={180} position="relative">
           <Flex
-            justifyContent="flex-end"
             alignItems="center"
             bgColor="#DCE0E0"
             width="50rem"
